38334a4fd41fcc64d4e0cd0cde66fff649fb7606
Maxime Rossini <maxime.rossini@gmail.com>
2014-01-18 03:19:09 +0100

    - Configured django for use with postgresql (via psycopg2) - Added requirement psycopg2 - Registered empty app 'about'

--- django_site/manage.py
+++ django_site/manage.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 import os
 import sys
+import glob
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "madmox_website.settings")
+    env_dir = 'envdir'
+    env_vars = glob.glob(os.path.join(env_dir, '*'))
+    for env_var in env_vars:
+        with open(env_var, 'r') as env_var_file:
+            os.environ.setdefault(env_var.split(os.sep)[-1],
+                                  env_var_file.read().strip())
 
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
