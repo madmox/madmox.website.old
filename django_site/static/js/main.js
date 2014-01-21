82994eab5638cd0bb801aef3761aa227414c38b0
Maxime Rossini <maxime.rossini@gmail.com>
2014-01-21 02:43:28 +0100

    Implementation of 'about' app and base templates
    - about/ improved
    - about/cv implemented
    - Navigation improved
    - Style rewrited

---
+++ django_site/static/js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    var all_ul = $("#accordion").children('ul');
+    var active_ul = $("#accordion li.active").parent();
+    var index = all_ul.index(active_ul);
+    $( "#accordion" ).accordion({
+        heightStyle: "content",
+        active: index
+    });
+});
