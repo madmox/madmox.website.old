2f21045c094b07af8daf11f80ca5d69d86d38a71
Maxime Rossini <maxime.rossini@gmail.com>
2014-02-05 02:33:48 +0100

    Updated global design for about and recipes apps

--- django_site/static/js/main.js
+++ django_site/static/js/main.js
@@ -1,6 +1,6 @@
 $(function() {
     var all_ul = $("#accordion").children('ul');
-    var active_ul = $("#accordion li.active").parent();
+    var active_ul = $("#accordion .active").parent();
     var index = all_ul.index(active_ul);
     $( "#accordion" ).accordion({
         heightStyle: "content",
