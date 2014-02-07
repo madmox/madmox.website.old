bdafdb2ca5a03ef5677cdbea0a9bb0a90eb97d2d
Maxime Rossini <maxime.rossini@gmail.com>
2014-02-07 04:37:34 +0100

    Bugfix: when side navigation has no active entry, the last node is expanded instead of the good one

--- django_site/static/js/main.js
+++ django_site/static/js/main.js
@@ -1,6 +1,6 @@
 $(function() {
     var all_ul = $("#accordion").children('ul');
-    var active_ul = $("#accordion .active").parent();
+    var active_ul = $("#accordion > ul.active");
     var index = all_ul.index(active_ul);
     $( "#accordion" ).accordion({
         heightStyle: "content",
