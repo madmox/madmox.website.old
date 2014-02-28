fec8240d3dab23e9c439187697fbe640b6284215
Maxime Rossini <maxime.rossini@gmail.com>
2014-02-28 01:25:49 +0100

    ajout des templates de gestion du compte

--- django_site/accounts/urls.py
+++ django_site/accounts/urls.py
@@ -12,4 +12,52 @@ urlpatterns = patterns('',
         'django.contrib.auth.views.logout',
         name='logout'
     ),
+    url(
+        r'^password/change/$',
+        'django.contrib.auth.views.password_change',
+        {
+            'template_name': 'accounts/password_change.html',
+            'post_change_redirect': 'accounts:password-change-done'
+        },
+        name='password-change'
+    ),
+    url(
+        r'^password/change/done/$',
+        'django.contrib.auth.views.password_change_done',
+        {'template_name': 'accounts/password_change_done.html'},
+        name='password-change-done'
+    ),
+    url(
+        r'^password/reset/$',
+        'django.contrib.auth.views.password_reset',
+        {
+            'template_name': 'accounts/password_reset.html',
+            'email_template_name': 'accounts/password_reset_email.html',
+            'subject_template_name': 'accounts/password_reset_subject.txt',
+            'post_reset_redirect': 'accounts:password-reset-done',
+            'from_email': 'madmox.fr <noreply@madmox.fr>'
+        },
+        name='password-reset'
+    ),
+    url(
+        r'^password/reset/done/$',
+        'django.contrib.auth.views.password_reset_done',
+        {'template_name': 'accounts/password_reset_done.html'},
+        name='password-reset-done'
+    ),
+    url(
+        r'^password/reset/confirm/(?P<uidb64>[^/]+)/(?P<token>[^/]+)/$',
+        'django.contrib.auth.views.password_reset_confirm',
+        {
+            'template_name': 'accounts/password_reset_confirm.html',
+            'post_reset_redirect': 'accounts:password-reset-complete'
+        },
+        name='password-reset-confirm'
+    ),
+    url(
+        r'^password/reset/complete/$',
+        'django.contrib.auth.views.password_reset_complete',
+        {'template_name': 'accounts/password_reset_complete.html'},
+        name='password-reset-complete'
+    ),
 )
