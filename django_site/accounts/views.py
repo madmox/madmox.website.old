d46194484bc3308cb1f87088c7fb706071a86f82
Maxime Rossini <maxime.rossini@gmail.com>
2014-03-08 17:41:20 +0100

    implemented profile update form

--- django_site/accounts/views.py
+++ django_site/accounts/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from accounts.forms import UserChangeForm
 
 
 @login_required
 def update_account(request, template_name):
-    return render(request, template_name, {})
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, template_name, { 'form': form })
