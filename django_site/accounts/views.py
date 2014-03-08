4a395441292843e2b7c169d2bcf417ff4053ff9c
Maxime Rossini <maxime.rossini@gmail.com>
2014-03-08 23:29:43 +0100

    no cache on external proxies

--- django_site/accounts/views.py
+++ django_site/accounts/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_control
 from accounts.forms import UserChangeForm
 
 
 @login_required
+@cache_control(no_cache=True, no_store=True, must_revalidate=True)
 def update_account(request, template_name):
     if request.method == 'POST':
         form = UserChangeForm(request.POST, instance=request.user)
@@ -19,5 +21,6 @@ def update_account(request, template_name):
     return render(request, template_name, { 'form': form })
 
 @login_required
+@cache_control(no_cache=True, no_store=True, must_revalidate=True)
 def update_account_done(request, template_name):
     return render(request, template_name, {})
