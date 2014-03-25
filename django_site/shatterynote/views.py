3211c0fce62ce6d9b67c910adb9b941c0661b625
Maxime Rossini <maxime.rossini@gmail.com>
2014-03-25 17:00:33 +0100

    removed unused import

--- django_site/shatterynote/views.py
+++ django_site/shatterynote/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views.decorators.cache import cache_control
 
