1108124daaf8716020d6dff3dd4d79b7514f1dd0
Maxime Rossini <maxime.rossini@gmail.com>
2014-03-31 16:21:08 +0200

    corrected unit error (kio -> ko)

--- django_site/share/templatetags/share_filters.py
+++ django_site/share/templatetags/share_filters.py
@@ -4,15 +4,15 @@ register = template.Library()
 
 @register.filter
 def print_size(value):
-    if value < 1024:
+    if value < 1000:
         result = '{0}   '.format(value) + '  o'
-    elif value < 1024 ** 2:
-        result = '{0:.2f}'.format(value / 1024) + ' Ko'
-    elif value < 1024 ** 3:
-        result = '{0:.2f}'.format(value / 1024 ** 2) + ' Mo'
-    elif value < 1024 ** 4:
-        result = '{0:.2f}'.format(value / 1024 ** 3) + ' Go'
+    elif value < 1000 ** 2:
+        result = '{0:.2f}'.format(value / 1000) + ' Ko'
+    elif value < 1000 ** 3:
+        result = '{0:.2f}'.format(value / 1000 ** 2) + ' Mo'
+    elif value < 1000 ** 4:
+        result = '{0:.2f}'.format(value / 1000 ** 3) + ' Go'
     else:
-        result = '{0:.2f}'.format(value / 1024 ** 4) + ' To'
+        result = '{0:.2f}'.format(value / 1000 ** 4) + ' To'
     
     return result
