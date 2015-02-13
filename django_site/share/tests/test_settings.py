a287552cc0e99c68d8a02c087f06db48d22e2d5c
Maxime Rossini <maxime.rossini@gmail.com>
2015-02-14 00:38:37 +0100

    refactored share app

--- django_site/share/tests/test_settings.py
+++ django_site/share/tests/test_settings.py
@@ -1,7 +1,22 @@
 from core.tests.utils import SettingsTestsBase
+import os.path
+import posixpath
 
 
 class SettingsTests(SettingsTestsBase):
 
     def test_share_setting_share(self):
-        self.check_environment_key('DJANGO_AES_KEY')
+        try:
+            from share import settings
+        except Exception as e:
+            self.fail("Could not import settings module: {0}".format(str(e)))
+        
+        # Assert path is absolute and exists
+        self.assertTrue(os.path.isabs(settings.SHARE_ROOT))
+        self.assertTrue(os.path.exists(settings.SHARE_ROOT))
+        self.assertTrue(os.path.isdir(settings.SHARE_ROOT))
+
+        # Path should be written POSIX style to avoid display issues
+        path_posix = posixpath.normpath(settings.SHARE_ROOT)
+        self.assertTrue(settings.SHARE_ROOT == path_posix)
+        
--- django_site/share/utils.py
+++ django_site/share/utils.py
@@ -1,25 +1,15 @@
+import os.path
+import posixpath
+import sys
 try:
     import magic
 except ImportError:
     import mimetypes
-import os.path
-import re
-import sys
 
 from share.settings import SHARE_ROOT
 
 
 # Classes
-class DoesNotExist(Exception):
-    pass
-    
-class IsNotFileOrDirectory(Exception):
-    pass
-
-class IsNotFile(Exception):
-    pass
-
- 
 class FileSystemNode:
     """
     A mapping between a physical directory in the shared folder and metadata
@@ -27,27 +17,30 @@ class FileSystemNode:
     """
     
     def __init__(self, path, set_children=True):
-        if not os.path.exists(path):
-            raise DoesNotExist("The path {0} does not exist.".format(path))
-        
         self.path = path
         self.isfile = os.path.isfile(path)
         self.isdir = os.path.isdir(path)
-        if not (self.isfile or self.isdir):
-            raise IsNotFileOrDirectory(
-                "The path {0} is not a file or a directory.".format(path)
-            )
-        
         self.isroot = (path == SHARE_ROOT)
         if self.isroot:
             self.name = 'share'
+            self.display_path = '/'
         else:
-            self.name = os.path.basename(path.rstrip('/\\'))
-        self.local_path = self.build_local_path()
+            self.name = os.path.basename(path)
+            self.display_path = (
+                posixpath.join('/', posixpath.relpath(path, start=SHARE_ROOT))
+            )
+        self.size = self.get_size()
         self.url = self.build_url()
         self.parent_url = self.build_parent_url()
-        self.size = self.get_size()
-        self.children = self.get_children(set_children)
+        if self.isfile:
+            self.mime_type = self.get_mime_type()
+            self.children = []
+        else:
+            self.mime_type = None
+            if set_children:
+                self.children = self.get_children()
+            else:
+                self.children = []
     
     def get_size(self):
         """
@@ -61,104 +54,68 @@ class FileSystemNode:
                     total_size += os.path.getsize(fp)
         else:
             total_size = os.path.getsize(self.path)
-            
+        
         return total_size
         
-    def get_children(self, set_children):
-        child_dirs, child_files = [], []
-        if self.isdir and set_children:
-            for filename in sorted(os.listdir(self.path), key=str.lower):
-                escaped_filename = filename.encode('utf8','surrogateescape').decode('utf8')
-                abspath = os.path.join(self.path, escaped_filename)
-                if os.path.isdir(abspath):
-                    child_dirs.append(FileSystemNode(abspath, set_children=False))
-                if os.path.isfile(abspath):
-                    child_files.append(FileSystemNode(abspath, set_children=False))
-        
-        return child_dirs + child_files
-    
-    def build_local_path(self):
-        # Removes share root part from path
-        # We want users to see the share root as a virtual root
-        local_path = '/' + re.sub(
-            r'^{0}'.format(re.escape(SHARE_ROOT)),
-            r'',
-            self.path
-        ).replace('\\', '/').lstrip('/')
-        return local_path
-    
     def build_url(self):
-        # Removes share root part from path
-        # We want users to see the share root as a virtual root
-        relative_url = re.sub(
-            r'^{0}'.format(re.escape(SHARE_ROOT)),
-            r'',
-            self.path
-        ).replace('\\', '/').strip('/')
-        if self.isdir:
+        relative_url = self.display_path.lstrip('/')
+        # URL has trailing slash for directories
+        if not self.isroot and self.isdir:
             relative_url += '/'
-        
         return relative_url
 
     def build_parent_url(self):
         if self.isroot:
             return None
         else:
-            # Removes share root part from path
-            # We want users to see the share root as a virtual root
-            relative_url = re.sub(
-                r'^{0}'.format(re.escape(SHARE_ROOT)),
-                r'',
-                self.path
-            ).replace('\\', '/').strip('/')
-            relative_url = '/'.join(relative_url.split('/')[0:-1])
+            relative_url = posixpath.dirname(self.display_path).lstrip('/')
             if relative_url != '':
                 relative_url += '/'
-            
             return relative_url
 
+    def get_mime_type(self):
+        if 'magic' in sys.modules:
+            return magic.from_file(self.path, mime=True).decode('utf-8')
+        else:
+            return mimetypes.guess_type(self.path)[0]
+        
+    def get_children(self):
+        child_dirs, child_files = [], []
+        for child in sorted(os.listdir(self.path), key=str.lower):
+            abspath = posixpath.join(self.path, child)
+            if os.path.isdir(abspath):
+                child_dirs.append(FileSystemNode(abspath, set_children=False))
+            if os.path.isfile(abspath):
+                child_files.append(FileSystemNode(abspath, set_children=False))
+        
+        return child_dirs + child_files
+    
 
 # Helpers
-def get_mime_type(file_path):
-    if 'magic' in sys.modules:
-        return magic.from_file(file_path, mime=True).decode('utf-8')
-    else:
-        return mimetypes.guess_type(file_path)[0]
-
 def get_physical_path(path):
     """
-    Converts the path exracted from the URL to a physical path
+    Converts the path exracted from the URL to a physical path (if path exists)
     """
-    # Security against URL attacks
-    if ('..' in path or ':' in path or
-        path.startswith('/') or path.startswith('\\')):
-        return None, None, None
     
-    if path != '':
-        path = os.path.normpath(path.replace('\\', '/'))
-    path = os.path.normpath(os.path.join(SHARE_ROOT, path))
-    if os.path.exists(path):
-        if os.path.isdir(path):
-            path += os.path.normpath('/')
-        return path, os.path.isdir(path), os.path.isfile(path)
+    # Clean up given path to only allow serving files below SHARE_ROOT
+    path = posixpath.normpath(path).lstrip('/') # Normalize path Unix-style
+    filepath = ''
+    for segment in path.split('/'):
+        if not segment:
+            # Strip empty path components (usually if path is empty)
+            continue
+        # Remove drive segments on drive-enabled servers (Windows...)
+        drive, segment = os.path.splitdrive(segment)
+        # Remove possible backslashes in the segment on Windows-style systems
+        head, segment = os.path.split(segment)
+        # Strip remaining '.' and '..' in path
+        if segment in (os.curdir, os.pardir):
+            continue
+        # Append segment to file path, posix-style
+        filepath = posixpath.join(filepath, segment)
+    filepath = posixpath.normpath(posixpath.join(SHARE_ROOT, filepath))
+    
+    if os.path.isdir(filepath) or os.path.isfile(filepath):
+        return filepath
     else:
-        return None, None, None
-
-def get_file_infos(file_path):
-    """
-    Creates an HTTP response to stream the given file.
-    
-    Tries to guess the MIME type from mimetypes built-in module.
-    """
-    
-    if not os.path.exists(file_path):
-        raise DoesNotExist("The path {0} does not exist.".format(file_path))
-    if not os.path.isfile(file_path):
-        raise IsNotFile(
-            "The path {0} is not a file or a directory.".format(file_path)
-        )
-    file_name = os.path.basename(file_path)
-    file_size = os.path.getsize(file_path)
-    mime_type = get_mime_type(file_path) or 'application/octet-stream'
-    
-    return file_name, file_size, mime_type
+        return None
